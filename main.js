331698dda53bc458ebaee55fd4614ebac4a657ba
BondarAndrew <107639998+BondarAndrew@users.noreply.github.com>
2023-05-02 21:50:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// function getAge(year, peopleAge) {
+//     let getStartLive = year - peopleAge;
+//     return getStartLive;
+// }
+
+
+// let yearNow = Number(prompt('Enter year now:'));
+// let age = Number(prompt("Enter your age:"));
+
+
+// let result = getAge(yearNow, age);
+// alert(`you were born ${result}`);
+
+
+let fns = {
+    one: function() {
+        console.log('Hi')
+    },
+
+    two: function() {
+        console.log('bye')
+    }
+}
+
+
+fns.one();
+fns.two();
